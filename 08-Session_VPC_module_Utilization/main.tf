79f987990949ff8f763e06e9d6aea92ba9c12cd6
Fazal Shareef <fazalshareef@Fazals-MacBook-Air.local>
2025-10-29 13:49:36 +0530

    small_change

--- 08-Session_VPC_module_Utilization/main.tf
+++ 08-Session_VPC_module_Utilization/main.tf
@@ -1,7 +1,7 @@
 
 module "vpc" {
   source         = "git::https://github.com/Fazalshareef/terraform.git//07-Session_VPC_Module"
-  vpc_name       = "terrafrom_vpc"
+  vpc_name_tag       = "terraform_vpc"
   vpc_cidr       = "10.0.0.0/16"
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   azs            = ["us-east-1a", "us-east-1b"]
